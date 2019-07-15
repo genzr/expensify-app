9247065691c2dcdd7b1af9988b4666ad2a05aabd
genzr <genzr05@gmail.com>
2019-07-15 17:59:38 +1000

    Setup production webpack build

--- src/app.js
+++ src/app.js
@@ -3,18 +3,10 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore'
-import { addExpense } from './actions/expenses'
-import { setTextFilter } from './actions/filters'
-import getVisibleExpenses from './selectors/expenses'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 
 const store = configureStore()
-
-const state = store.getState()
-
-// use getVisibleExpenses function and print visible ones to screen
-console.log(getVisibleExpenses(state.expenses, state.filters))
 
 const jsx = (
     <Provider store={store}>
