2a55d05b3d1a36017bd095d9c465c9eb4a74c19f
genzr <genzr05@gmail.com>
2019-07-18 18:46:49 +1000

    removed unecessary google auth file and added further auth functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,24 @@
 import { NavLink } from "react-router-dom"
 import React from 'react'
+import { startLogout } from '../actions/auth'
+import { connect } from 'react-redux'
 
-const Header = () => (
+export const Header = ({ startLogout} ) => (
     <header>
         <h1>Expensify</h1>
         <NavLink to="/dashboard" activeClassName="is-active" exact={true}>Dashboard</NavLink>
         <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
         <NavLink to="/help" activeClassName="is-active">Help</NavLink>
+        <button onClick={startLogout}> Logout </button>
     </header>
 )
 
-export default Header
+const mapDispatchToProps = (dispatch) => {
+    return {
+        startLogout: () => {
+            dispatch(startLogout())
+        }
+    }
+}
+
+export default connect(undefined, mapDispatchToProps)(Header)
